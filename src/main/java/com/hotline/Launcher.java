91a0618eedfc31534e7d6b09f76ccae9836cb624
Joshua Murguia Garcia <pgsrcia5@gmail.com>
2025-11-19 17:13:58 -0600

    feat: added launcher to get executable

---
+++ src/main/java/com/hotline/Launcher.java
@@ -0,0 +1,8 @@
+package com.hotline;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Esta línea llama a tu clase principal real
+        App.main(args);
+    }
+}
